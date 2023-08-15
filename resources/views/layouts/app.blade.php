55d322f3ebabda81f1e8ec7baccf17f47efa4a4a
Mehmet Can Özcan <mcozcan@outlook.com.tr>
2023-08-15 13:45:05 +0300

    Bootstrap 5 kurulumu tamamlandı
    Bootstrap 5 kurulumu tamamlandı

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap Kurulumu Tammalandı</title>
+    <link href="{{asset('front/css/bootstrap.min.css')}}" rel="stylesheet">
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+
+    <script src="{{asset('front/js/bootstrap.min.js')}}" ></script>
+  </body>
+</html>
